ad73f384ff172da2b21b8bce28438aedf065c1da
“Tijmen <tgmeijer@student.tudelft.nl>
2025-05-06 15:13:31 +0200

    update python to 3.12

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='lib_version',
-    version='test_0.0.2',
+    version='0.0.2',
     description='Version-aware utility library',
     author='remla25-team13',
     packages=find_packages(),
-    python_requires='>=3.10',
+    python_requires='>=3.12',
 )
