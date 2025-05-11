0c083881cd37c6a58a6b2f3a0df93ceac7c025a7
“Tijmen <tgmeijer@student.tudelft.nl>
2025-05-11 19:25:21 +0200

    finalize version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lib_version',
-    version='0.0.2',
+    version='1.0.0',
     description='Version-aware utility library',
     author='remla25-team13',
     packages=find_packages(),
